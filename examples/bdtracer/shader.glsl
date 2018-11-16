1732c47f448eea4372361d47453ea42359b86299
Otto Seiskari <otto.seiskari@gmail.com>
2018-11-16 19:13:50 +0200

    generalized bdtracer code

--- examples/bdtracer/shader.glsl
+++ examples/bdtracer/shader.glsl
@@ -6,7 +6,7 @@ uniform vec2 resolution;
 uniform vec2 tent_filter;
 uniform vec3 random_direction_1, random_direction_2, random_direction_3;
 uniform vec3 light_sample;
-uniform float light_selection;
+uniform float light_selection, reflection_selection;
 uniform float frame_number;
 
 uniform sampler2D base_image;
@@ -66,6 +66,11 @@ vec2 get_ccd_pos(vec2 screen_pos) {
     return ((jittered_pos / resolution.xy) * 2.0 - 1.0) * vec2(1.0, aspect);
 }
 
+// tracer parameters
+#define N_BOUNCES 3
+#define IMAGE_BRIGHTNESS 100.0
+
+// secene geometry
 #define OBJ_NONE 0
 #define OBJ_SPHERE_1 1
 #define OBJ_SPHERE_2 2
@@ -76,11 +81,6 @@ vec2 get_ccd_pos(vec2 screen_pos) {
 
 #define N_LIGHTS 2
 
-#define REFLECTIVE_OBJECT OBJ_SPHERE_2
-
-#define N_BOUNCES 3
-
-#define IMAGE_BRIGHTNESS 100.0
 #define ROOM_H 2.0
 #define ROOM_W 5.0
 
@@ -92,11 +92,21 @@ vec2 get_ccd_pos(vec2 screen_pos) {
 #define BOX_SIZE (vec3(ROOM_W, ROOM_W, ROOM_H)*0.5)
 #define BOX_CENTER vec3(0.0, 0.0, ROOM_H*0.5)
 
-#define light_1_pos vec3(-ROOM_W*0.5, 0.0, ROOM_H)
-#define light_2_pos vec3(0.0, ROOM_W*0.5, ROOM_H)
 #define light_r 0.4
 
+#define light_1_pos vec3(-ROOM_W*0.5, 0.0, ROOM_H)
+#define light_2_pos vec3(0.0, ROOM_W*0.5, ROOM_H)
+
+// materials
+#define light_1_emission vec3(0.8, 0.8, 1.0)
+#define light_2_emission vec3(1.0, 0.8, 0.6)
+
+#define sphere_1_diffuse vec3(.5, .8, .9)
+#define box_diffuse vec3(1., 1., 1.)*.7
+
+// helpers
 #define UNIT_SPHERE_AREA (4.0*M_PI)
+#define ZERO_VEC3 vec3(0.0, 0.0, 0.0)
 
 int find_intersection(vec3 ray_pos, vec3 ray, int prev_object, out vec4 intersection) {
     int which_object = OBJ_NONE;
@@ -144,6 +154,52 @@ int find_intersection(vec3 ray_pos, vec3 ray, int prev_object, out vec4 intersec
     return which_object;
 }
 
+int select_light(out vec3 light_point, out float sample_prob_density_per_area) {
+      light_point = normalize(light_sample) * light_r;
+      sample_prob_density_per_area = 1.0 / (UNIT_SPHERE_AREA*light_r*light_r * float(N_LIGHTS));
+
+      int light_object = OBJ_NONE;
+      if (light_selection > 0.5) {
+        light_point += light_1_pos;
+        light_object = OBJ_LIGHT_1;
+      } else {
+        light_point += light_2_pos;
+        light_object = OBJ_LIGHT_2;
+      }
+
+      return light_object;
+}
+
+bool get_emission(int which_obj, out vec3 emission) {
+  if (which_obj == OBJ_LIGHT_1) {
+    emission = light_1_emission;
+  } else if (which_obj == OBJ_LIGHT_2) {
+    emission = light_2_emission;
+  } else {
+    emission = ZERO_VEC3;
+    return false;
+  }
+  return true;
+}
+
+vec3 get_diffuse(int which_object) {
+  if (which_object == OBJ_SPHERE_1 || which_object == OBJ_SPHERE_2) {
+    return sphere_1_diffuse;
+  } else if (which_object == OBJ_BOX) {
+    return box_diffuse;
+  } else {
+    return ZERO_VEC3;
+  }
+}
+
+float get_reflectivity(int which_object) {
+  if (which_object == OBJ_SPHERE_2) {
+    return 1.0;
+  } else {
+    return 0.0;
+  }
+}
+
 float weight1(float p1, float p2) {
     //return 0.5;
     return p1 / (p1 + p2); // balance heuristic
@@ -153,13 +209,11 @@ float weight1(float p1, float p2) {
 
 #define weight2 weight1
 
-void main() {
-    // scene geometry
-    const vec3 light_1_emission = vec3(0.8, 0.8, 1.0);
-    const vec3 light_2_emission = vec3(1.0, 0.8, 0.6);
-    const vec3 sphere_1_diffuse = vec3(.5, .8, .9);
-    const vec3 box_diffuse = vec3(1., 1., 1.)*.7;
+// uncomment to use pure path tracing
+//#define weight1(a,b) 1.0
+//#define weight2(a,b) 0.0
 
+void main() {
     // define camera
     const float fov_angle = DEG2RAD(50.0);
     const float cam_theta = DEG2RAD(300.0);
@@ -175,19 +229,11 @@ void main() {
     cam_y = cross(cam_x, cam_z);
     cam_pos = -cam_z * cam_dist + camera_target;
 
-    vec3 light_point = normalize(light_sample) * light_r;
-    float light_sample_area_probability = 1.0 / (UNIT_SPHERE_AREA*light_r*light_r * float(N_LIGHTS));
-    int light_object = OBJ_NONE;
+    vec3 light_point;
+    float light_sample_area_probability;
     vec3 light_emission;
-    if (light_selection > 0.5) {
-      light_point += light_1_pos;
-      light_object = OBJ_LIGHT_1;
-      light_emission = light_1_emission;
-    } else {
-      light_point += light_2_pos;
-      light_object = OBJ_LIGHT_2;
-      light_emission = light_2_emission;
-    }
+    int light_object = select_light(light_point, light_sample_area_probability);
+    get_emission(light_object, light_emission);
 
     // ray location on image surface after applying tent filter
     vec2 ccd_pos = get_ccd_pos(gl_FragCoord.xy);
@@ -197,35 +243,31 @@ void main() {
     vec3 ray_color = vec3(1.0, 1.0, 1.0) * IMAGE_BRIGHTNESS;
     int prev_object = OBJ_NONE;
 
-    vec3 cur_color = vec3(0.0, 0.0, 0.0);
+    vec3 cur_color = ZERO_VEC3;
+    bool was_diffuse = false;
+    float ray_type_sample = reflection_selection;
 
     for (int bounce = 0; bounce <= N_BOUNCES; ++bounce)  {
 
         // find intersection
         vec4 intersection; // vec4(normal.xyz, distance)
-        vec3 diffuse = vec3(0,0,0);
 
         int which_object = find_intersection(ray_pos, ray, prev_object, intersection);
 
         if (which_object == OBJ_NONE) {
-            ray_color = vec3(0.0, 0.0, 0.0);
+            ray_color = ZERO_VEC3;
         } else {
 
             vec3 normal = intersection.xyz;
             ray_pos += intersection.w * ray;
 
-            vec3 emission = vec3(0.0, 0.0, 0.0);
-            if (which_object == OBJ_LIGHT_1) {
-              emission = light_1_emission;
-            } else if (which_object == OBJ_LIGHT_2) {
-              emission = light_2_emission;
-            }
-            if (emission.x > 0.0) {
+            vec3 emission = ZERO_VEC3;
+            if (get_emission(which_object, emission)) {
                 float invDist2 = 1.0 / (intersection.w*intersection.w);
                 float probThis = 2.0 * invDist2 / UNIT_SPHERE_AREA * -dot(normal, ray);
                 float probOther = light_sample_area_probability;
 
-                if (prev_object == REFLECTIVE_OBJECT || bounce == 0) {
+                if (!was_diffuse) {
                     probOther = 0.0;
                     probThis = 1.0;
                 }
@@ -249,19 +291,19 @@ void main() {
             }
 
             float directionCoeff = 0.0;
-            if (which_object == REFLECTIVE_OBJECT) {
+            float reflectivity = get_reflectivity(which_object);
+            if (ray_type_sample < reflectivity) {
+                ray_type_sample /= reflectivity;
+
                 // full reflection
                 ray = ray - 2.0*dot(normal, ray)*normal;
                 directionCoeff = 1.0;
+                was_diffuse = false;
             } else {
+                ray_type_sample -= reflectivity;
+                ray_type_sample /= 1.0 - reflectivity;
 
-                if (which_object == OBJ_SPHERE_1) {
-                  diffuse = sphere_1_diffuse;
-                } else if (which_object == OBJ_BOX) {
-                  diffuse = box_diffuse;
-                } else {
-                  diffuse = vec3(0.0, 0.0, 0.0);
-                }
+                // diffuse reflection
                 // sample a new direction
                 vec3 rand_dir;
                 if (bounce == 0) rand_dir = random_direction_1;
@@ -272,10 +314,11 @@ void main() {
                 if (dot(ray, normal) < 0.0) ray = -ray;
                 directionCoeff = dot(normal, ray);
 
-                ray_color *= diffuse;
+                ray_color *= get_diffuse(which_object);
+                was_diffuse = true;
             }
 
-            if (bounce < N_BOUNCES && which_object != REFLECTIVE_OBJECT && (
+            if (bounce < N_BOUNCES && was_diffuse && (
                   shadow_object == OBJ_NONE ||
                   (shadow_object == light_object && dot(shadow_isec.xyz, shadow_ray) < 0.0) ||
                   shadow_isec.w > shadow_dist)) {
